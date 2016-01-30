5a537908eac4d2cda887a7d5316c61db03de7ff7
3noch <elliot.cameron@level12.io>
2016-01-30 11:00:32 -0500

    Initial implementation

---
+++ README.md
@@ -0,0 +1,10 @@
+serve-here
+==========
+
+A minimal server for static content.
+
+
+Development
+-----------
+
+You need [Haskell stack](http://docs.haskellstack.org/). Then run `stack setup && stack build`.
---
+++ src/Main.hs
@@ -0,0 +1,45 @@
+module Main where
+
+import Data.Maybe (maybe)
+import Servant (Proxy(..), serve, Server)
+import Servant.API (Raw)
+import Servant.Utils.StaticFiles (serveDirectory)
+import Network.Wai (Application)
+import qualified Network.Wai.Handler.Warp as Warp
+import Options.Applicative
+
+type Api = Raw
+
+mainServer :: Options -> IO ()
+mainServer (Options dir port) = Warp.run port $
+  serve (Proxy :: Proxy Api) (serveDirectory dir)
+
+data Options = Options
+  { _dir  :: String
+  , _port :: Int
+  } deriving (Eq, Show)
+
+main :: IO ()
+main = execParser opts >>= mainServer
+  where
+    opts = info (helper <*> options)
+      ( fullDesc
+     <> progDesc "Serve the contents of DIRECTORY on HTTP port PORT"
+     <> header "serve-here - A minimal server for static content" )
+
+options :: Parser Options
+options = Options
+  <$> strOption
+      ( long "directory"
+     <> short 'd'
+     <> metavar "DIRECTORY"
+     <> value "."
+     <> showDefault
+     <> help "Serve contents of DIRECTORY" )
+  <*> option auto
+      ( long "port"
+     <> short 'p'
+     <> metavar "PORT"
+     <> value 8001
+     <> showDefault
+     <> help "Listen on HTTP port PORT" )
